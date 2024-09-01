724fe66d61cb728b40f3d16a0045dbd6a306ac94
Babita Verma <147910203+babitaverma07@users.noreply.github.com>
2024-09-01 09:30:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,49 @@
-/* Edit this file */
-const player = document.querySelector('.player');
-const video = player.querySelector('.viewer');
-const progress = player.querySelector('.progress');
-const progressBar = player.querySelector('.progress__filled');
-const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelectorAll('.player__slider');
+// Selecting elements
+const video = document.querySelector('.video__player');
+const progress = document.querySelector('.progress');
+const progressBar = document.querySelector('.progress__filled');
+const playButton = document.querySelector('.player__button');
+const volumeInput = document.querySelector('.volume');
+const playbackSpeedInput = document.querySelector('.playbackSpeed');
+const skipBackwardButton = document.querySelector('.skip-backward');
+const skipForwardButton = document.querySelector('.skip-forward');
+
+// Adding event listeners
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('play', togglePlayButton);
+video.addEventListener('pause', togglePlayButton);
+playButton.addEventListener('click', togglePlay);
+volumeInput.addEventListener('input', setVolume);
+playbackSpeedInput.addEventListener('input', setPlaybackSpeed);
+skipBackwardButton.addEventListener('click', skipBackward);
+skipForwardButton.addEventListener('click', skipForward);
+
+// Functions
+function updateProgress() {
+  const progressPercent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${progressPercent}%`;
+}
+
+function togglePlayButton() {
+  playButton.textContent = video.paused ? '►' : '❚ ❚';
+}
+
+function togglePlay() {
+  video.paused ? video.play() : video.pause();
+}
+
+function setVolume() {
+  video.volume = volumeInput.value;
+}
+
+function setPlaybackSpeed() {
+  video.playbackRate = playbackSpeedInput.value;
+}
+
+function skipBackward() {
+  video.currentTime -= 10;
+}
+
+function skipForward() {
+  video.currentTime += 25;
+}
